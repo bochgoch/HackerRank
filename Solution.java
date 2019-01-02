9f8fbf7cc52b3e3aefe14e914886ae61e446e450
bochgoch <martin@bochgoch.com>
2019-01-02 11:42:44 +0000

    Add files via upload
    Read input from STDIN. 
    Print output to STDOUT.

---
+++ Solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+              Scanner sc=new Scanner(System.in);
+              int T=sc.nextInt();
+              while(T-->0){
+                  int ele=sc.nextInt();
+                  System.out.println(ele);
+              }
+       }
+}

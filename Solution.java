7d3d57d74bcd216d590083663797d27d9ee09a85
bochgoch <martin@bochgoch.com>
2019-01-03 16:39:39 +0000

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,14 +1,27 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Solution {
 
-    public static void main(String[] args) {
-              Scanner sc=new Scanner(System.in);
-              int T=sc.nextInt();
-              while(T-->0){
-                  int ele=sc.nextInt();
-                  System.out.println(ele);
-              }
-       }
+    static boolean isAnagram(String a, String b) {
+        String aA[] = a.split("");
+        String bA[] = b.split("");
+     
+        Arrays.sort(aA);
+        Arrays.sort(bA);
+        
+        if (Arrays.toString(aA).compareTo(Arrays.toString(bA))==0) return true;
+        return false;
+
+    }
+
+  public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
 }
